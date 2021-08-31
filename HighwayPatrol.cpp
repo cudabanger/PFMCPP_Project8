80feb6b07d66763bfde74d3bc1d0b3fa8dd5c46b
cudabanger <88297671+cudabanger@users.noreply.github.com>
2021-08-31 00:38:49 +0000

    Forgot to implement the request to add the name in the pullOver function.

--- HighwayPatrol.cpp
+++ HighwayPatrol.cpp
@@ -35,7 +35,7 @@ void HighwayPatrol::pullOver( Vehicle* v, bool willArrest, Highway* h )
     if( willArrest )
     {
         //print the vehicle type in this std::cout between "THE [" and "] PULL". 
-        std::cout << name << ": YOU IN THE [ " << " ] PULL OVER AND SHOW YOUR HANDS" << std::endl;
+        std::cout << name << ": YOU IN THE " << v->name << " PULL OVER AND SHOW YOUR HANDS" << std::endl;
         std::cout << "EVERYONE ELSE, SLOW DOWN!! \n\n\n";
         h->removeVehicle(v);
     }
